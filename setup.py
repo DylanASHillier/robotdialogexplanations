5b56a2944d53710b4b4aec212f3370b48c92dd18
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-04-01 15:49:54 +0100

    adds fetch command for conceptnet csv

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 import urllib.request
+import gzip
 
 if __name__ == '__main__':
-    urllib.request.urlretrieve("https://storage.googleapis.com/gresearch/kelm-corpus/updated-2021/kelm_generated_corpus.jsonl","Datasets/KELMCorpus/kelm_generated_corpus.jsonl")
+    urllib.request.urlretrieve("https://storage.googleapis.com/gresearch/kelm-corpus/updated-2021/kelm_generated_corpus.jsonl","datasets/KELMCorpus/kelm_generated_corpus.jsonl")
+    urllib.request.urlretrieve("https://dumps.wikimedia.org/wikidatawiki/entities/latest-truthy.nt.gz","datasets/KGs/wikidata_dump.gz")
+    urllib.request.urlretrieve("https://s3.amazonaws.com/conceptnet/downloads/2018/edges/conceptnet-assertions-5.6.0.csv.gz","datasets/KGs/conceptnetassertions.csv.gz")
