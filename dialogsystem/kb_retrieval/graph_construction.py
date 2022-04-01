234d1442c1b4753ae1870d83c7d83a8ef977bc4d
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-04-01 15:48:00 +0100

    updates the graph construction algorithms to be useful for knowledge graphs

--- dialogsystem/kb_retrieval/graph_construction.py
+++ dialogsystem/kb_retrieval/graph_construction.py
@@ -1,12 +1,34 @@
-from networkx import compose_all, ego_graph
+from multiprocessing import Pool
+from networkx import subgraph, single_source_shortest_path_length, compose
 from networkx import Graph
+
+def extract_nodes(tup):
+    nxg,ec,k = tup
+    lengths = single_source_shortest_path_length(nxg, ec, cutoff=k)
+    return list(lengths.keys())
 
 class GraphConstructor():
     def __init__(self) -> None:
         self.graph = Graph()
 
     def input_nx_graph_with_trimming(self,nxgraph,entity_candidates,k):
-        self.graph = compose_all([ego_graph(nxgraph,entity,radius=k) for entity in entity_candidates]+[self.graph])
+        pool = Pool()
+        nodes = pool.map(extract_nodes,[(nxgraph,ec,k) for ec in entity_candidates])
+        pool.close()
+        pool.join()
+        nodes = [node for nodelist in nodes for node in nodelist]
+        self.graph = compose(self.graph,subgraph(nxgraph,nodes))
+
+    def extract_relevant_nodes(self,nxgraph,background_entities,k=1):
+        '''
+        this method is for getting the label data for any potentially relevant entities
+        '''
+        pool = Pool()
+        nodes = pool.map(extract_nodes,[(nxgraph,ec,k) for ec in background_entities])
+        pool.close()
+        pool.join()
+        nodes = [node for nodelist in nodes for node in nodelist]
+        return nodes
 
     def add_from_triple(self,triple):
         '''
