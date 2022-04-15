65ce20e35b5ef29c62c211a97a219e49f6686250
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-04-15 11:28:35 +0100

    finalises dataset machinations

---
+++ dialogsystem/data/graphDataset.py
@@ -0,0 +1,74 @@
+import os
+from typing import List, Tuple, Union
+from torch_geometric.data import Dataset
+from networkx.readwrite import gpickle
+from torch import save, load
+from models.GraphEmbedder import GraphTransformer
+from data import qtext
+from re import search
+from tqdm import tqdm
+
+def get_index(graphname):
+    match = search('\d+',graphname)
+    if match:
+        return int(match.group())
+    else:
+        raise ValueError(f"{graphname} does not contain a number")
+
+class GraphTrainDataset(Dataset):
+    def __init__(self,root="datasets/processed_ds", raw_filepath='datasets', datasets = ['ropes','coqa','squad'],graph_transformation_LM='google/byt5-small',transform=None, pre_transform=None, pre_filter=None) -> None:
+        """
+        graph_transformation_LM
+        """
+        self.graphembedder=GraphTransformer(graph_transformation_LM)  
+        self.init_jsons = []
+        self.raw_filepath = raw_filepath
+        for ds in datasets:
+            if ds=='ropes':
+                text_ds = qtext.QtextRopes("train")
+            elif ds=='squad':
+                text_ds = qtext.QtextSQUAD("train")
+            elif ds=='coqa':
+                text_ds = qtext.QtextCoQA("train")
+            graph_files = os.listdir(raw_filepath+'/'+ds)
+            self.init_jsons += [(text_ds[get_index(e)][0],raw_filepath+'/'+ds+'/'+e)  for i,e in enumerate(graph_files)]
+        super(GraphTrainDataset, self).__init__(root,transform,pre_transform,pre_filter)
+
+
+    @property
+    def processed_file_names(self) -> Union[str, List[str], Tuple]:
+        return [f"data_{i}.pt" for i,_ in enumerate(self.init_jsons)]
+
+    @property
+    def raw_dir(self) -> str:
+        return os.path.join(self.root, self.name, 'raw')
+
+    @property          
+    def raw_file_names(self) -> Union[str, List[str], Tuple]:
+        return self.init_jsons
+
+    def process(self):
+        idx = 0
+        for query, graph in tqdm(self.init_jsons):
+            sample = gpickle.read_gpickle(graph)
+            graph, _ = self.graphembedder(sample)
+            data = self.graphembedder.add_query(graph,query)
+            
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+
+            save(data, os.path.join(self.processed_dir, f'data_{idx}.pt'))
+            idx += 1
+
+    def get(self, idx):
+        return load(os.path.join(self.processed_dir, f'data_{idx}.pt'))
+
+    def len(self):
+        return len(self.init_jsons)
+
+if __name__ == '__main__':
+    gtds = GraphTrainDataset()
+    print(gtds[0])
--- dialogsystem/models/GraphEmbedder.py
+++ dialogsystem/models/GraphEmbedder.py
@@ -5,26 +5,37 @@ from torch import cat, tanh, mean
 from networkx import compose_all, ego_graph, get_edge_attributes, get_node_attributes
 from torch_geometric.utils import from_networkx
 from torch_geometric.transforms import LineGraph
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+from tqdm import tqdm
 
 class LMEmbedder(Module):
-    '''
-    Used to embed text using a language model. This is based off of 'Sentence-T5: Scalable Sentence Encoders
-    from Pre-trained Text-to-Text Models', Ni et al. 2021
-    Uses the encoder of the input encoder-decoder language model to create an encoding by taking th emean of the encoder outputs across all input tokens
-    Arguments:
-        lm: a hugging face transformers Encoder-Decoder model
-        tokenizer: the tokenizer
-    Note that this is intended to be frozen!
-    '''
-    def __init__(self, lm, tokenizer):
-        super().__init__()
+    def __init__(self, lm, tokenizer, max_batchsize=128):
+        '''
+        Used to embed text using a language model. This is based off of 'Sentence-T5: Scalable Sentence Encoders
+        from Pre-trained Text-to-Text Models', Ni et al. 2021
+        Uses the encoder of the input encoder-decoder language model to create an encoding by taking th emean of the encoder outputs across all input tokens
+        Arguments:
+            lm: a hugging face transformers Encoder-Decoder model
+            tokenizer: the tokenizer
+        Note that this is intended to be frozen!
+        '''
+        super(LMEmbedder,self).__init__()
         self.encoder = lm
         self.tokenizer = tokenizer
+        self.max_batchsize=max_batchsize
 
     def forward(self, text):
-        ttext = self.tokenizer(text)
-        output = self.encoder(ttext)
-        return mean(output[0])
+        if type(text) is str:
+            ttext = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True).input_ids
+            output = self.encoder(ttext)
+            return mean(output[0])
+        else:
+            texts = [text[i:i+self.max_batchsize] for i in range(0,len(text),self.max_batchsize)]
+            outputs = []
+            for text in tqdm(texts):
+                ttext = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True).input_ids
+                outputs.append(self.encoder(ttext)[0])
+            return mean(cat(outputs))
         
 
 class JointEmbedder(Module):
@@ -34,7 +45,7 @@ class JointEmbedder(Module):
     Note that query and graph_text are embedded into initial_embedding_size (the output of an LM)    
     '''
     def __init__(self, initial_embedding_size,embedding_size,hidden_dim):
-        super().__init__()
+        super(JointEmbedder,self).__init__()
         self.layers = ModuleList()
         self.layers.append(Linear(initial_embedding_size*2,hidden_dim))
         for i in range(2):
@@ -48,8 +59,41 @@ class JointEmbedder(Module):
         return x
 
 class GraphTransformer(Module):
-    def __init__(self,) -> None:
-        self.graph_embedder = GraphDataEmbedder()
+    def __init__(self,lm_string="google/byt5-small") -> None:
+        """
+        lm_string: name of encoder decoder language model used for embedding
+        """
+        super(GraphTransformer,self).__init__()
+        lmodel = T5ForConditionalGeneration.from_pretrained(lm_string).encoder
+        tokenizer = AutoTokenizer.from_pretrained(lm_string)
+        self.lm_embedder = LMEmbedder(lmodel,tokenizer)
+
+    def embed(self, nxgraph):
+        '''
+        Returns the node and edge attributes of the result of embedding the graph 
+        Arguments:
+            graph: networkx graph instance,
+            query: string, query used
+        '''
+        # node_attributes = get_node_attributes(nxgraph,'label')
+        node_attributes = {node:node for node in nxgraph.nodes} ## due to error not saving node attributes as labels
+        keys = node_attributes.keys()
+        values = [node_attributes[key] for key in keys]
+        values = self.lm_embedder(values)
+        node_attributes = {key: values[i] for i,key in enumerate(keys)}
+        # node_attributes = {key: self.lm_embedder(value) for key,value in node_attributes.items()}
+
+        # for node in node_attributes:
+        #     new_node_attributes[node]=self.lm_embedder(node_attributes[node])
+        edge_attributes = get_edge_attributes(nxgraph,'label')
+        keys = edge_attributes.keys()
+        values = [edge_attributes[key] for key in keys]
+        values = self.lm_embedder(values)
+        edge_attributes = {key: values[i] for i,key in enumerate(keys)}
+        # edge_attributes = {key: self.lm_embedder(value) for key, value in edge_attributes.items()}
+        # for edge in edge_attributes:
+        #     new_edge_attributes[edge]=self.lm_embedder(edge_attributes[edge])
+        return node_attributes,edge_attributes        
 
     def forward(self,nxgraph):
         '''
@@ -61,31 +105,15 @@ class GraphTransformer(Module):
 
         use the edge_index to reverse the line graph transform (for checking the text on the original triple).
         '''
-        node_attributes, edge_attributes = self.graph_embedder(nxgraph)
+        node_attributes, edge_attributes = self.embed(nxgraph)
         for edge in edge_attributes: # combine embeddings for nodes and edges
             edge_attributes[edge]=cat[edge_attributes[edge],node_attributes[edge[0]],node_attributes[edge[1]]]
         data = from_networkx(nxgraph,None,edge_attributes)
         edge_index = data.edge_index
         data = LineGraph()(data)
         return data, edge_index
-        
-class GraphDataEmbedder(Module):
-    def __init__(self, lm_embedding_size, lm_embedder, embedding_size):
-        super().__init__()
-        self.lm=lm_embedder
-        self.nodeEmbedder = JointEmbedder(lm_embedding_size, embedding_size)
-        self.edgeEmbedder = JointEmbedder(lm_embedding_size, embedding_size)
 
-    def forward(self, graph, query):
-        '''
-        Arguments:
-            graph: networkx graph instance,
-            query: string, query used
-        '''
-        node_attributes = get_node_attributes(graph)
-        for node in node_attributes:
-            node_attributes[node]=self.node_embedder(node_attributes[node],query)
-        edge_attributes = get_edge_attributes(graph)
-        for edge in edge_attributes:
-            edge_attributes[edge]=self.edge_embedder(edge_attributes[edge],query)
-        return node_attributes,edge_attributes
+    def add_query(self, graph_data, query):
+        query_embedding = self.lm_embedder(query)
+        graph_data.x = cat([graph_data.x,query_embedding.expand((graph_data.x.size(0),-1))], dim=1) ## concats on query embedding
+        return graph_data
