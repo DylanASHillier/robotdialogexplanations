64787e95b0f9efb8ec00b3504f52192cf1674604
Ubuntu <ubuntu@ip-172-31-7-130.eu-west-2.compute.internal>
2022-04-25 12:12:28 +0000

    updates model for new embedding technique

--- dialogsystem/models/kgqueryextract.py
+++ dialogsystem/models/kgqueryextract.py
@@ -6,7 +6,7 @@ from pytorch_lightning import LightningModule
 from torch.optim import Adam
 
 class LightningKGQueryMPNN(LightningModule):
-    def __init__(self, embedding_size, hidden_dim=50, k=20, num_layers=3, lr=1e-3, heads=4):
+    def __init__(self, embedding_size=None, hidden_dim=50, k=20, num_layers=4, lr=1e-3, heads=4):
         '''
         Operates over a graph to obtain the indices of edges that should be used in the knowledge graph
         Arguments:
@@ -16,16 +16,18 @@ class LightningKGQueryMPNN(LightningModule):
         '''
         super(LightningKGQueryMPNN,self).__init__()
         self.final_layer = Linear(hidden_dim,1)
-        self.init_layer = Linear(embedding_size,hidden_dim)
+        if embedding_size is not None:
+            self.init_layer = Linear(embedding_size,hidden_dim)
         self.hidden_layers = ModuleList([GATv2Conv(hidden_dim,hidden_dim,heads=heads, concat=False, dropout=0.2,) for i in range(num_layers)])
-        self.activations = [ELU() for i in range(num_layers-1)]+[lambda x:x]
+        self.activations = [ELU() for i in range(num_layers)]
         self.k = k
         self.lr = lr
         self.loss = MSELoss()
         self.save_hyperparameters()
 
     def forward(self, x, edge_index):
-        x = self.init_layer(x)
+        if self.init_layer:
+            x = self.init_layer(x)
         for i,layer in enumerate(self.hidden_layers):
             x = layer(x,edge_index)
             x = self.activations[i](x)
@@ -37,7 +39,8 @@ class LightningKGQueryMPNN(LightningModule):
         x = batch.x
         y = batch.y
         edge_index = batch.edge_index
-        x = self.init_layer(x)
+        if self.init_layer:
+            x = self.init_layer(x)
         for i,layer in enumerate(self.hidden_layers):
             x = layer(x,edge_index)
             x = self.activations[i](x)
