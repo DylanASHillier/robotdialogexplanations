e594e80eec963f5b616c72d76400a8b0414880a2
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-04-27 22:25:39 +0100

    adds graph visualisations

--- dialogsystem/visualise_graph.py
+++ dialogsystem/visualise_graph.py
@@ -2,11 +2,51 @@ from networkx.readwrite import gpickle
 from networkx import draw
 from matplotlib import pyplot
 
-graph = gpickle.read_gpickle(f"datasets/ropes/graphtest.json")
-print(graph)
-print(graph.nodes(data=True))
-print([edge[2]["relevance_label"] for edge in graph.edges(data=True)])
-# print(list(graph.nodes)[:100])
-print([edge for edge in graph.edges(data=True) if edge[2]["relevance_label"]>1])
-draw(graph,with_labels=False)
-# pyplot.show()
+graph = gpickle.read_gpickle(f"logs/basegraph0.json")
+from dash import Dash, html
+import dash_cytoscape as cyto
+cyto.load_extra_layouts()
+
+node_elements = [
+    {'data': {'id': node, 'label': node}} for node in graph.nodes
+]
+edge_elements = [
+    {'data': {'source': u, 'target': v, 'label': label}} for u, v, label in graph.edges.data('label')
+]
+### plot in dash
+app = Dash(__name__)
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-compound-layout',
+        layout={'name': 'spread'},
+        style={'width': '100%', 'height': '1000px'},
+        elements=node_elements + edge_elements,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'content': 'data(label)',
+                    'text-opacity': 0.5,
+                    'text-valign': 'center',
+                    'text-halign': 'right',
+                    'background-color': '#11479e'
+                }
+            },
+            {
+                'selector': 'edge',
+                'style': {
+                    'width': 3,
+                    'target-arrow-shape': 'triangle',
+                    'content': 'data(label)',
+                    'line-color': '#9dbaea',
+                    'target-arrow-color': '#9dbaea',
+                    'curve-style': 'bezier',
+                    'target-arrow-fill': 'filled',
+                    'label': 'data(label)'
+                }
+            }
+        ]
+    )
+])
+
+app.run_server(debug=True)
