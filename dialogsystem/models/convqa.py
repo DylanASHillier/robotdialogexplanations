b3522896777e36ca978a00a64b3eaf7eef49f601
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-05-03 17:04:29 +0100

    updates how convqa is loaded

--- dialogsystem/models/convqa.py
+++ dialogsystem/models/convqa.py
@@ -1,17 +1,19 @@
 from pytorch_lightning import LightningModule
-from transformers import AutoTokenizer, BertForQuestionAnswering, T5ForConditionalGeneration
-from torch import mean, stack, no_grad
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+from torch import no_grad
 from torch.optim import Adam
 
 class ConvQASystem(LightningModule):
     '''
-    NB this is never actually trained by me, we just use a lightning wrapper for convenience
+    NB this is never actually trained using this class, we just use a lightning wrapper for convenience
+    Arguments:
+        checkpoint_path: path to the checkpoint to load (hugging face checkpoint)
     '''
-    def __init__(self,base_model="bert-base-uncased",lr=1e-5):
+    def __init__(self,checkpoint_path):
         super(ConvQASystem,self).__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained(base_model)
-        self.model = BertForQuestionAnswering.from_pretrained(base_model)
-        self.lr = lr
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
+        self.model = T5ForConditionalGeneration.from_pretrained(checkpoint_path)
+        self.lr = 1e-5
         self.save_hyperparameters()
 
     def forward(self,input):
@@ -23,12 +25,6 @@ class ConvQASystem(LightningModule):
             outputs = self.model.generate(input)
             outputs = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
             return outputs
-
-    def load_from_hf_checkpoint(self, checkpoint_path):
-        # self.model = BertForQuestionAnswering.from_pretrained(checkpoint_path)
-        self.model = T5ForConditionalGeneration.from_pretrained(checkpoint_path)
-        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-        return self
 
     def configure_optimizers(self):
         return Adam(self.parameters(), self.lr)
