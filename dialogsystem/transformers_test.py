c248fb0d491f0ae6323993a4ffe78fd93d641f12
CatThePlatewright <catherine.ning9999@gmail.com>
2022-08-26 12:54:34 +0100

    added script for testing different pre trained language models for t2t

--- dialogsystem/transformers_test.py
+++ dialogsystem/transformers_test.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # gpt-neo-1.3B
 # RUCAIBox/mvp (returns gibberish, even tried in format "Describe the data: robot|likes|apples"), google/pegasus (which one to choose? documentation is not very clear)
 # "gpt2" # returns code ? not NL sentence? gptj too large
-model_string = "EleutherAI/gpt-neo-1.3B"  #BEST
+model_string = "EleutherAI/gpt-neo-2.7B"  #BEST
 
 #robot, is, big; robot, like, apples
 #robot, visited, waypoint; waypoint, is, kitchen
@@ -20,12 +20,13 @@ model = AutoModelForCausalLM.from_pretrained(model_string)
 tokenizer = AutoTokenizer.from_pretrained(model_string)
 pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=-1)
 generation_params={
-"temperature":0.05,
+"temperature":0.5,
 "do_sample":True,
 "top_p":0.4,
 "repetition_penalty":1.5,
 "length_penalty":0.01,
-"max_length":200, #or max_new_tokens?
+#"max_length":150, 
+"max_new_tokens": 50
 # could have top_k = 50 activated too
  
 }
@@ -47,7 +48,7 @@ generation_params={
 if __name__=='__main__':
     while True:
         input_triple = input("Enter a sentence: ")
-        input_string = f"translate triples to text: tiago, is, robot; robot, holds, box; box, is, red -> tiago holds a red box\n robot,in,shower;robot,is,singing -> a robot is singing in the shower\n robot, at, wp2 ; robot, is, blue -> a blue robot is at wp2\n robot, in, kitchen; kitchen, has, table -> the robot is in the kitchen with a table {input_triple}->"
+        input_string = f"translate triples to text: tiago, is, robot; robot, holds, box; box, is, red -> tiago holds a red box\n robot,in,shower;robot,is,singing -> a robot is singing in the shower\n robot, at, wp2 ; robot, is, blue -> a blue robot is at wp2\n robot,is in, kitchen; kitchen, has, table -> a robot is in the kitchen with a table \n box, is, red; box, is in, kitchen; robot, holds, box; robot, is, blue -> A blue robot is holding a red box in the kitchen{input_triple}->"
         out = pipeline(
         input_string, 
         return_full_text=False,
